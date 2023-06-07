51769cdd9d87bd23c9d89d9e87422ce4a2531109
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-06-07 14:33:28 -0600

    delete exercises

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,3 +1,5 @@
+TRUNCATE albums;
+
 INSERT INTO albums (artist, name, release_date, sales, genre)
 VALUES ('Michael Jackson', 'Thriller', 1982, 51.2,'Pop'),
        ('AC/DC', 'Back in Black', 1980, 30.1,'Hard Rock'),
---
+++ delete_exercises.sql
@@ -0,0 +1,11 @@
+SELECT * FROM albums where release_date > 1991;
+
+SELECT * FROM albums WHERE genre = 'Disco';
+
+SELECT * FROM albums WHERE artist = 'Whitney Houston';
+
+DELETE FROM albums where release_date > 1991;
+
+DELETE FROM albums WHERE genre = 'Disco';
+
+DELETE FROM albums WHERE artist = 'Whitney Houston';
