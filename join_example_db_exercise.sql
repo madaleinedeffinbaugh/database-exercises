49bc24ed50f9259fe2047c62bc09b2bf3e8121e9
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-06-09 19:43:02 -0600

    join example db 1.3

--- join_example_db_exercise.sql
+++ join_example_db_exercise.sql
@@ -35,3 +35,7 @@ VALUES ('jake', 'jake@example.com', 1),
 SELECT users.name as user_name, roles.name as role_name
 FROM users
 JOIN roles ON users.role_id = roles.id;
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+LEFT JOIN roles ON users.role_id = roles.id;
