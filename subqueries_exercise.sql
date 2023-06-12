84a4a18d59494065b6465bde7368ccafc0f9bb94
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-06-12 15:00:22 -0600

    subqueries exercise complete

---
+++ subqueries_exercise.sql
@@ -0,0 +1,19 @@
+SELECT * FROM employees
+WHERE hire_date IN (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = 101010);
+
+SELECT title
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM  employees
+    WHERE first_name = 'Aamod');
+
+SELECT first_name, last_name
+FROM employees WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date = '9999-01-01')
+AND gender = 'F';
